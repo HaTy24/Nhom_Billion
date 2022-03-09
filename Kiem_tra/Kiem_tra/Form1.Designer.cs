86ba647f53fbfa5298fd818f0e97e1c84bca062d
Billion <hathienty24@gmail.com>
2022-03-09 08:49:33 +0700

    add_txtmatkhau

--- Kiem_tra/Kiem_tra/Form1.Designer.cs
+++ Kiem_tra/Kiem_tra/Form1.Designer.cs
@@ -31,13 +31,14 @@ namespace Kiem_tra
         {
             this.label1 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(116, 74);
+            this.label1.Location = new System.Drawing.Point(44, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 0;
@@ -45,26 +46,34 @@ namespace Kiem_tra
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(190, 71);
+            this.txtUserName.Location = new System.Drawing.Point(119, 68);
             this.txtUserName.Name = "txtUserName";
             this.txtUserName.Size = new System.Drawing.Size(153, 20);
             this.txtUserName.TabIndex = 1;
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(173, 139);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mật Khẩu";
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(348, 68);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(153, 20);
+            this.txtmatkhau.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(623, 235);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -78,7 +87,8 @@ namespace Kiem_tra
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUserName;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtmatkhau;
     }
 }
 
